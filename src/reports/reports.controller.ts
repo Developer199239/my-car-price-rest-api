135bd8e5707b3ca586f7e0ab8bb1734701085ab7
Murtuza Rahman <murtuza.rahman83@gmail.com>
2022-11-28 16:50:33 +0600

    Relations with TypeORM

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGurad } from 'src/guards/auth.guard';
+import { Serialize } from 'src/interceptors/serialize.interceptors';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportService: ReportsService) {}
+
+    @Post()
+    @UseGuards(AuthGurad)
+    @Serialize(ReportDto)
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User){
+        return this.reportService.create(body, user);
+    }
+}
